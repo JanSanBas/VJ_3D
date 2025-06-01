8d91744ad1276a2b00d85b74c4361b08c9594310
JanSanBas <jan.santos@estudiantat.upc.edu>
2025-06-01 17:21:03 +0200

    feature/Modelos3DPowerUps: hechos

--- Projecte/Assets/Scripts/ScriptPowerUp.cs
+++ Projecte/Assets/Scripts/ScriptPowerUp.cs
@@ -22,89 +22,14 @@ public class PowerUpItem : MonoBehaviour
         rb.useGravity = false;
         rb.isKinematic = true;
 
-        SetupAppearance();
-
         StartCoroutine(MoveDown());
-    }
-
-    private void SetupAppearance()
-    {
-        Renderer renderer = GetComponent<Renderer>();
-        if (renderer != null)
-        {
-            switch (powerUpType)
-            {
-                case PowerUpType.PowerBall:
-                    renderer.material.color = Color.red;
-                    break;
-                case PowerUpType.BigPaddle:
-                    renderer.material.color = Color.green;
-                    break;
-                case PowerUpType.SmallPaddle:
-                    renderer.material.color = Color.blue;
-                    break;
-                case PowerUpType.NormalBall:
-                    renderer.material.color = Color.cyan;
-                    break;
-                case PowerUpType.Magnet:
-                    renderer.material.color = Color.yellow;
-                    break;
-                case PowerUpType.ExtraLife:
-                    renderer.material.color = Color.magenta;
-                    break;
-                case PowerUpType.NextLevel:
-                    renderer.material.color = Color.black;
-                    break;
-                case PowerUpType.GodMode: // �Nuevo caso!
-                    renderer.material.color = new Color(1f, 0.5f, 0f); // Naranja o Dorado
-                    break;
-                case PowerUpType.Rocket:
-                    renderer.material.color = new Color(0.8f, 0.4f, 0f); // Color marr�n/naranja oscuro
-                    break;
-            }
-        }
-
-        TextMesh textMesh = GetComponentInChildren<TextMesh>();
-        if (textMesh != null)
-        {
-            switch (powerUpType)
-            {
-                case PowerUpType.PowerBall:
-                    textMesh.text = "P";
-                    break;
-                case PowerUpType.BigPaddle:
-                    textMesh.text = "B";
-                    break;
-                case PowerUpType.SmallPaddle:
-                    textMesh.text = "S";
-                    break;
-                case PowerUpType.NormalBall:
-                    textMesh.text = "N";
-                    break;
-                case PowerUpType.Magnet:
-                    textMesh.text = "M";
-                    break;
-                case PowerUpType.ExtraLife:
-                    textMesh.text = "+1";
-                    break;
-                case PowerUpType.NextLevel:
-                    textMesh.text = "NL";
-                    break;
-                case PowerUpType.GodMode: // �Nuevo caso!
-                    textMesh.text = "GM"; // Texto para God Mode
-                    break;
-                case PowerUpType.Rocket:
-                    textMesh.text = "R"; // Texto para Rocket
-                    break;
-            }
-        }
     }
 
     private IEnumerator MoveDown()
     {
         while (transform.position.z > -10f) // L�mite inferior
         {
-            transform.Translate(Vector3.back * fallSpeed * Time.deltaTime);
+            transform.Translate(Vector3.back * fallSpeed * Time.deltaTime, Space.World);
             yield return null;
         }
 
@@ -121,6 +46,7 @@ public class PowerUpItem : MonoBehaviour
             }
             Destroy(gameObject);
         }
+        else return;
     }
 
     private void OnCollisionEnter(Collision collision)
@@ -133,5 +59,6 @@ public class PowerUpItem : MonoBehaviour
             }
             Destroy(gameObject);
         }
+        else return;
     }
 }
